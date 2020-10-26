735f3ad4b39aa224958d7fdd4adcd220eab37eb1
BohdanKalymon <72624509+BohdanKalymon@users.noreply.github.com>
2020-10-26 15:43:39 +0300

    Add files via upload

---
+++ laba41/laba41/Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream> 
+#include <cmath> 
+using namespace std;
+int main() {
+    int i;     double P;
+
+    cout << "i = "; cin >> i;
+
+    P = 1;     i = 1;     while (i <= 15)
+    {
+        P *= pow(sin(i), 2) + pow(cos(1. / i), 2.) / i * i;         i++;
+    }
+    cout << P << endl; 
+
+    P = 1;     i = 1;     do {
+        P *= pow(sin(i), 2) + pow(cos(1. / i), 2.) / i * i;         i++;
+    } while (i <= 15);     cout << P << endl;
+
+    P = 1;
+    for (i = 1; i <= 15; i++)
+    {
+        P *= pow(sin(i), 2) + pow(cos(1. / i), 2.) / i * i;
+    }
+    cout << P << endl;
+
+    P = 1;
+    for (i = 15; i >= 1; i--)
+    {
+        P *= pow(sin(i), 2) + pow(cos(1. / i), 2.) / i * i;
+    }
+    cout << P << endl; 
+
+    return 0;
+}
+
+
